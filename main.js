8a6c1456228d232ae7344754edfcf5be98bb5acf
maysamrashed <maysamrashed20@gmail.com>
2024-07-14 04:47:51 +0300

    jsProject

---
+++ main.js
@@ -0,0 +1,18 @@
+const getProduct = async () => {
+    const { data } = await axios.get("https://dummyjson.com/products");
+    return data;
+}
+const displayProducts = async () => {
+    const data = await getProduct();
+    const result = data.products.map((product) => {
+        return `
+        <div class= "product">
+        <h2>${product.title}</h2>
+        <img src="${product.thumbnail}"/>
+        <a href="details.html?id=${product.id}">details</a>
+        </div>
+        `;
+    }).join(" ");
+    document.querySelector(".products").innerHTML = result;
+}
+displayProducts();
